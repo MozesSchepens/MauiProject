6c60d0a18beabf4e523557de546fca0d16830f47
MozesSchepens <91198219+MozesSchepens@users.noreply.github.com>
2025-01-13 13:46:10 +0100

    Add files via upload

---
+++ MauiProgram.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Maui;
+using Microsoft.Maui.Controls.Hosting;
+using Microsoft.Maui.Hosting;
+using MauiProject.Services;
+
+namespace MauiProject
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+            builder.Services.AddSingleton<DatabaseService>();
+
+            return builder.Build();
+        }
+    }
+}
---
+++ views/AddEventPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Maui.Controls;
+using MauiProject.Models;
+using MauiProject.Services;
+using System.Collections.ObjectModel;
+
+namespace MauiProject.Views
+{
+    public partial class AddEventPage : ContentPage
+    {
+        private readonly DatabaseService _databaseService;
+        private readonly ObservableCollection<Event> _events;
+
+        // Default constructor (nodig voor de XAML-compiler)
+        public AddEventPage()
+        {
+            InitializeComponent();
+        }
+
+        // Constructor met parameters
+        public AddEventPage(DatabaseService databaseService, ObservableCollection<Event> events)
+        {
+            InitializeComponent();
+            _databaseService = databaseService;
+            _events = events;
+        }
+
+        private async void OnSaveClicked(object sender, EventArgs e)
+        {
+            var newEvent = new Event
+            {
+                Name = NameEntry.Text,
+                Date = DatePicker.Date,
+                Location = LocationEntry.Text,
+                Description = DescriptionEditor.Text
+            };
+
+            await _databaseService.SaveEventAsync(newEvent);
+            _events.Add(newEvent);
+            await Navigation.PopAsync();
+        }
+    }
+}
